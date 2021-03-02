dc455fa2fd6b52a482b3dc642fa5af670bd9770a
Ocielrs123 <ociel_rs@hotmail.com>
2021-03-02 15:23:02 -0600

    commituno

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import GoogleMap from './components/GoogleMap';
+import {Helmet} from "react-helmet";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Google Maps App</h1>
+      <GoogleMap />
+      <Helmet>
+      <script defer data-key="0a944a45-2192-4b71-adab-f9cd3e8782d6" src="https://widget.tochat.be/bundle.js"></script>
+       </Helmet>
+    </div>
+  );
+}
+
+export default App;
